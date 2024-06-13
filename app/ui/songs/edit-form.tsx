68ac8304eec3796abae2a48e2ceab37f7561ac20
nbp <bits@squirrelradio.com>
2024-06-12 22:21:31 -0700

    Update legacy link

--- app/ui/songs/edit-form.tsx
+++ app/ui/songs/edit-form.tsx
@@ -231,7 +231,7 @@ export default function EditSongForm({
       </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
-          href={`http://system:6969/songs/${song.id}/edit`}
+          href={`http://192.168.193.76:6969/songs/${song.id}/edit`}
           className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
         >
           Legacy
