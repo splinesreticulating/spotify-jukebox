365da4d680496e35c936e45085885f016ba10167
nbp <bits@squirrelradio.com>
2024-06-14 09:59:36 -0700

    Remove unused import

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,7 +1,6 @@
 'use server'
 
 import { z } from 'zod'
-import { sql } from '@vercel/postgres'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 import { signIn } from '@/auth'
