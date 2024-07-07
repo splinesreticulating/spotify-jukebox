db6edaeba36b45d41f416a0e7ebf8e4296e78cab
nbp <bits@squirrelradio.com>
2024-07-07 14:11:38 -0700

    Add potential support for historylist_last

--- app/lib/components/LastPlayed.tsx
+++ app/lib/components/LastPlayed.tsx
@@ -2,7 +2,28 @@ import { Song } from '../definitions'
 import { daysAgo } from '../utils'
 
 export const LastPlayed: React.FC<{ song: Song }> = ({ song }) => {
-    const days = daysAgo(song.date_played!)
+    // const lastPlayedDate = await checkHistorylists(song)
+    const lastPlayedDate = song.date_played!
+    const days = daysAgo(lastPlayedDate)
     
     return (<em>last played {days} days ago</em>)
 }
+
+// :thinkface:
+// const checkHistorylists = async (song: Song) => {
+//     let lastPlayedDate = new Date('00-00-0000')
+
+//     const semiRecent = await db.historylist.findFirst({ where: { songID: song.id }, orderBy: { songID: 'desc'} })
+
+//     console.log( { semiRecent })
+    
+//     if (!semiRecent) {
+//         const notSoRecent = await db.historylist_last.findFirst({ where: { songID: song.id }, orderBy: { songID: 'desc'} })
+//         console.log( { notSoRecent })
+
+//         if (notSoRecent) lastPlayedDate = notSoRecent.date_played
+//     }
+
+
+//     return lastPlayedDate
+// }
