138e8d54b22a8df9907c30213db0642dd024c478
nbp <nbp@squirrelradio.com>
2024-12-04 17:31:13 -0800

    FIx hydration error

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useTransition } from 'react'
 import Link from 'next/link'
 import NavLinks from '@/app/ui/dashboard/nav-links'
 import JukeBoxLogo from '@/app/ui/jukeBox-logo'
@@ -7,6 +8,14 @@ import { PowerIcon } from '@heroicons/react/24/outline'
 import { handleSignOut } from '@/app/lib/actions/auth'
 
 export default function SideNav() {
+  const [isPending, startTransition] = useTransition()
+
+  const handleSubmit = () => {
+    startTransition(() => {
+      handleSignOut()
+    })
+  }
+
   return (
     <div className="flex h-full flex-col px-4 py-4 sm:px-6 md:px-3">
       <Link
@@ -20,12 +29,13 @@ export default function SideNav() {
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-        <form action={handleSignOut}>
-          <button className="flex h-12 w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-2 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-            <PowerIcon className="w-5 sm:w-6" />
-            <div className="block">Sign Out</div>
-          </button>
-        </form>
+        <button
+          onClick={handleSubmit}
+          className="flex h-12 w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-2 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+        >
+          <PowerIcon className="w-5 sm:w-6" />
+          <div className="block">Sign Out</div>
+        </button>
       </div>
     </div>
   )
