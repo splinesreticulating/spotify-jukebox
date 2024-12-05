ce84408dd947107e51b29f42cc4b71bacfc426b2
nbp <nbp@squirrelradio.com>
2024-12-04 17:08:22 -0800

    Separate signout function

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,8 +1,10 @@
+'use client'
+
 import Link from 'next/link'
 import NavLinks from '@/app/ui/dashboard/nav-links'
 import JukeBoxLogo from '@/app/ui/jukeBox-logo'
 import { PowerIcon } from '@heroicons/react/24/outline'
-import { signOut } from '@/auth'
+import { handleSignOut } from '@/app/lib/actions/auth'
 
 export default function SideNav() {
   return (
@@ -18,12 +20,7 @@ export default function SideNav() {
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-        <form
-          action={async () => {
-            'use server'
-            await signOut()
-          }}
-        >
+        <form action={handleSignOut}>
           <button className="flex h-12 w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-2 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
             <PowerIcon className="w-5 sm:w-6" />
             <div className="block">Sign Out</div>
