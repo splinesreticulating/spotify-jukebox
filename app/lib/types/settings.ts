b847bdf292865a30354b1d7e64b93180e17cefba
nbp <nbp@squirrelradio.com>
2024-12-08 13:27:01 -0800

    Remove unused types

--- app/lib/types/settings.ts
+++ app/lib/types/settings.ts
@@ -3,10 +3,3 @@ export type Setting = {
   value: string
   description: string | null
 }
-
-export type SettingConfig = {
-  type: 'number' | 'text' | 'boolean'
-  options?: Array<{ value: string; label: string }>
-}
-
-export type SettingConfigs = Record<string, SettingConfig>
--- app/ui/settings/table.tsx
+++ app/ui/settings/table.tsx
@@ -5,7 +5,7 @@ import { toast } from 'sonner'
 import { toastStyles } from '@/app/lib/constants/toast-styles'
 import { useDebounce } from 'use-debounce'
 import clsx from 'clsx'
-import type { Setting, SettingConfig, SettingConfigs } from '@/app/lib/types'
+import type { Setting } from '@/app/lib/types'
 
 const formatSettingName = (name: string): string => {
   return name
