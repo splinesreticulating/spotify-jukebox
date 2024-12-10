a851ecdd95bf6d9a4c6ab01cc94e4ec8054f5ebc
nbp <nbp@squirrelradio.com>
2024-12-10 10:08:48 -0800

    Await more params

--- app/api/settings/[name]/route.ts
+++ app/api/settings/[name]/route.ts
@@ -3,9 +3,10 @@ import { NextResponse } from 'next/server'
 
 export async function PUT(request: Request, { params }: { params: { name: string } }) {
   try {
+    const { name } = await params
     const { value } = await request.json()
     const updatedSetting = await db.settings.update({
-      where: { name: params.name },
+      where: { name },
       data: {
         value,
         updated_at: new Date(),
