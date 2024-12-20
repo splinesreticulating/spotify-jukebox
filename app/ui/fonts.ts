459b8df1bb40c03e43f76b303883a886960ff253
nbp <nbp@squirrelradio.com>
2024-12-20 13:25:19 -0800

    Don't preload fonts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,16 @@
 import { Inter, Open_Sans } from 'next/font/google'
 
-export const inter = Inter({ subsets: ['latin'] })
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  preload: false,
+  variable: '--font-inter',
+})
+
 export const openSans = Open_Sans({
   subsets: ['latin'],
   weight: ['400', '700'],
+  display: 'swap',
+  preload: false,
+  variable: '--font-open-sans',
 })
